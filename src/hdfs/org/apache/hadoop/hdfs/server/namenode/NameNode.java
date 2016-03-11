e33551724ba12f9ec53603fbb094466d46d45045
developerdong <925844024@qq.com>
2016-03-11 10:02:57 +0800

    change placement log

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -121,7 +121,7 @@ public class NameNode implements ClientProtocol, DatanodeProtocol,
 
     public static final Log LOG = LogFactory.getLog(NameNode.class.getName());
     public static final Log stateChangeLog = LogFactory.getLog("org.apache.hadoop.hdfs.StateChange");
-    public static final Log placementLog = LogFactory.getLog("org.apache.hadoop.hdfs.placement");
+    public static final Log placementLog = LogFactory.getLog(">>>>>>>>>> org.apache.hadoop.hdfs.Placement <<<<<<<<<<");
     public FSNamesystem namesystem; // TODO: This should private. Use getNamesystem() instead.
     /** RPC server */
     private Server server;
