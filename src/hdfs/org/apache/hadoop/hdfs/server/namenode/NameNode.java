a39dd4fbb5449d1135574c1ff61112745e7cf29b
developerdong <925844024@qq.com>
2016-03-11 10:02:19 +0800

    change allocation log

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -121,7 +121,7 @@ public class NameNode implements ClientProtocol, DatanodeProtocol,
 
     public static final Log LOG = LogFactory.getLog(NameNode.class.getName());
     public static final Log stateChangeLog = LogFactory.getLog("org.apache.hadoop.hdfs.StateChange");
-    public static final Log allocationLog = LogFactory.getLog("org.apache.hadoop.hdfs.Allocation");
+    public static final Log allocationLog = LogFactory.getLog(">>>>>>>>>> org.apache.hadoop.hdfs.Allocation <<<<<<<<<<");
     public FSNamesystem namesystem; // TODO: This should private. Use getNamesystem() instead.
     /** RPC server */
     private Server server;
