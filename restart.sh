8729934ecdc78063320f258a8f416c1c815c52a7
developerdong <925844024@qq.com>
2016-03-27 22:10:29 +0800

    multi nodes cluster configure

--- restart.sh
+++ restart.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 bin/stop-all.sh
 ant
-rm -rf /tmp/hadoop-dong/dfs/data
-bin/hadoop namenode -format
-rm -rf logs/* 
+#rm -rf /tmp/hadoop-dong/dfs/data
+#bin/hadoop namenode -format
+rm -rf logs/*
+#scp -r build dong@192.168.1.29:/home/dong/DynamicReplication/
+ 
 bin/start-all.sh
 jps
