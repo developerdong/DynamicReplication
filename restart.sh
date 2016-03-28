4731d5a7c6ca7482e8ff9b929f68a267ee4a883a
developerdong <925844024@qq.com>
2016-03-28 21:40:40 +0800

    when restart cluster ,reformat

--- restart.sh
+++ restart.sh
@@ -4,6 +4,19 @@ ant
 #rm -rf /tmp/hadoop-dong/dfs/data
 #bin/hadoop namenode -format
 ssh dong@192.168.1.29 "rm -rf /home/dong/DynamicReplication/logs/*"
+
+ssh dong@192.168.1.29 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.30 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.31 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.32 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.33 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.34 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.35 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.36 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.37 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.38 "rm -rf /home/dong/DynamicReplication/tmp/*"
+ssh dong@192.168.1.39 "rm -rf /home/dong/DynamicReplication/tmp/*"
+
 scp -r build dong@192.168.1.29:/home/dong/DynamicReplication/
 scp -r build dong@192.168.1.30:/home/dong/DynamicReplication/
 scp -r build dong@192.168.1.31:/home/dong/DynamicReplication/
@@ -16,6 +29,7 @@ scp -r build dong@192.168.1.37:/home/dong/DynamicReplication/
 scp -r build dong@192.168.1.38:/home/dong/DynamicReplication/
 scp -r build dong@192.168.1.39:/home/dong/DynamicReplication/
 
+ssh dong@192.168.1.29 "/home/dong/DynamicReplication/bin/hadoop namenode -format"
  
 ssh dong@192.168.1.29 "sh /home/dong/DynamicReplication/bin/start-all.sh"
 
