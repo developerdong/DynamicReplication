30e7b516a2220eeb45355bff33f8ae8d0a65973c
developerdong <925844024@qq.com>
2016-03-28 12:01:06 +0800

    new restart.sh for multi nodes cluster

--- restart.sh
+++ restart.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-bin/stop-all.sh
+ssh dong@192.168.1.29 "sh /home/dong/DynamicReplication/bin/stop-all.sh"
 ant
-rm -rf /tmp/hadoop-dong/dfs/data
-bin/hadoop namenode -format
-rm -rf logs/* 
-bin/start-all.sh
-jps
+#rm -rf /tmp/hadoop-dong/dfs/data
+#bin/hadoop namenode -format
+ssh dong@192.168.1.29 "rm -rf /home/dong/DynamicReplication/logs/*"
+scp -r build dong@192.168.1.29:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.30:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.31:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.32:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.33:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.34:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.35:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.36:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.37:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.38:/home/dong/DynamicReplication/
+scp -r build dong@192.168.1.39:/home/dong/DynamicReplication/
+ 
+ssh dong@192.168.1.29 "sh /home/dong/DynamicReplication/bin/start-all.sh"
+
